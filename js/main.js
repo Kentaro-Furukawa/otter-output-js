6002b7aebad7c5184003ed2c125b2f805c165831
Kentaro Furukawa <info@ken89.com>
2023-08-08 21:45:00 +0900

    basic functions done.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const convertButton = document.querySelector("#convert");
+convertButton.addEventListener('click', () => {
+  handleConvert()
+})
+
+function handleConvert() {
+  const inputValue = document.querySelector("#input").value;
+  const outPut = document.querySelector("#output");
+  // get converted value
+  const CSVValue = convertToCSV(inputValue);
+  // show output
+  outPut.innerText = CSVValue
+}
+
+function convertToCSV(inputValue) {
+  // remove unwanted parts
+  const removeRegex = /^(\s*\|\s*)|(\s*\|\s*$)|(^\s*\+[+-\|]+\+\s*$)(\n)?/gm;
+  const valueRemvedFragments = inputValue.replace(removeRegex,"");
+  // put commas
+  const replaceToCommaRegex = /\s*\|\s*/gm;
+  const replacedPipesValue = valueRemvedFragments.replace(replaceToCommaRegex, ",");
+
+  return replacedPipesValue
+}
